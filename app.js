7696ab475dc4b5cef03cffd1c3835107edb5fbae
ameraz <ameraz@csumb.edu>
2019-07-30 04:08:32 -0400

    Initial commmit of Planets site

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+//root route
+app.get("/", function(req, res) {
+  //res.send("it works!");
+  res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+  res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+  res.render("venus.html");
+});
+
+//server listener
+app.listen("8081", "0.0.0.0", function(){
+  console.log("Express Server is Running...");
+});
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log("Running Express Server...");
+// });
