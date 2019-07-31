c7d18c5ee3ace1bccebe0a0a99aec2c7f0bc0354
ameraz <ameraz@csumb.edu>
2019-07-31 00:29:28 -0400

    Added all the requirements to the page

--- app.js
+++ app.js
@@ -17,11 +17,19 @@ app.get("/venus", function(req, res){
   res.render("venus.html");
 });
 
-//server listener
-// app.listen("8081", "0.0.0.0", function(){
-//   console.log("Express Server is Running...");
-// });
-
-app.listen(process.env.PORT, process.env.IP, function() {
-  console.log("Running Express Server...");
+app.get("/earth", function(req, res){
+  res.render("earth.html");
 });
+
+app.get("/references", function(req, res){
+  res.render("references.html");
+});
+
+//server listener
+app.listen("8081", "0.0.0.0", function(){
+  console.log("Express Server is Running...");
+});
+
+// app.listen(process.env.PORT, process.env.IP, function() {
+//   console.log("Running Express Server...");
+// });
